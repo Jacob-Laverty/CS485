67dd25f7c0cc056036f67d3cf30bb3146d79b5d6
Jacob-Laverty <jacob.laverty@gmail.com>
2014-11-07 00:23:15 -0500

    Task 1 almost 100%

--- Project3/Assignment3.lua
+++ Project3/Assignment3.lua
@@ -11,6 +11,7 @@ darwin = require('hfaDriver')
 
 func = function (hfa) end
 
+init_j = {}
 localize = {}
 faceXY = {}
 moveToXY = {}
@@ -18,48 +19,60 @@ stopRobot = {}
 
 xPrime = 0;
 yPrime = 0;
-aPrime = nil;
+aPrime = 0;
 
 
-behaviors = {localize, faceXY, moveToXY, stopRobot}
+behaviors = {init_j, localize, faceXY, moveToXY, stopRobot}
 states = {"start", "go", "stop"}
 
 for k,v in ipairs(behaviors) do
   for i,t in ipairs(states) do
     if v[t] == nil
     then
-      v[t] = func;
+      v[t] = function(hfa) end;
     end
   end
 end
 
 
+--Init start. Literally just move forward for a bit
+init_j["start"] = function(hfa) 
+	print("Init start");
+	darwin.lookGoal();
+	darwin.setVelocity(0.03,0, 0);
+end
+
 -- Look for the goal
 localize["start"] = function (hfa)
+  print("Localize start");
   darwin.lookGoal();
 end
 
 -- Find out where we are and where the target is relative to us
 faceXY["start"] = function(hfa)
-  vector = wcm.get_pose();
-  x = vector.x;
-  y = vector.y;
-  a = vector.a;
+  print("face start");
+  v = wcm.get_pose();
+  x = v.x;
+  y = v.y;
+  a = v.a;
 
   deltaX = xPrime - x;
   deltaY = yPrime - y;
 
   aPrime = math.atan2(deltaY, deltaX);
 
-  aPrime = aPrime * (180/math.pi);
+  --aPrime = aPrime * (180/math.pi);
 
   -- Rotate until we are facing where we want to be facing
-  darwin.setVelocity(0,0, 0.1);
+  print("I need to go to this angle: ");
+  print(aPrime);
+  darwin.setVelocity(0,0,0.1);
 end
 
 
 -- Move to the thing we are facing
 moveToXY["start"] = function(hfa) 
+  print("move start");
   -- Want to stop all prior movement
   darwin.stop();
   darwin.setVelocity(0.1,0,0);
@@ -67,6 +80,7 @@ end
 
 -- Stop the robot from doing shenanigans
 stopRobot["start"] = function(hfa)
+  print("stop start");
   darwin.setVelocity(0,0,0);
   darwin.stop();
 end
@@ -76,29 +90,63 @@ localize_b  = makeBehavior("localize", localize["start"], localize["go"], locali
 faceXY_b    = makeBehavior("faceXY", faceXY["start"], faceXY["go"], faceXY["stop"]);
 moveToXY_b  = makeBehavior("moveToXY", moveToXY["start"], moveToXY["go"], moveToXY["stop"]);
 stopRobot_b = makeBehavior("stopRobot", stopRobot["start"], stopRobot["go"], stopRobot["stop"]);
+init_jb 		= makeBehavior("init_j", init_j["start"], init_j["go"], init_j["stop"]);
 
 -- Make the HFA
 counter = 0;
+oldX = 0;
+oldY = 0;
+oldA = 0;
+
+startup = os.time();
 machine = makeHFA("machine", makeTransition({
-  [start] = localize_b,
+  [start] = init_jb,
+	[init_jb] = function() 
+		print("init_jb")
+		if os.difftime(os.time(), startup) > 15 then
+			return localize_b
+		else
+			return init_jb
+		end	
+	end,
+
   [localize_b] = function() -- if we're in localize
-    return faceXY_b -- assume we've updated our position
+		print("start");
+		v = wcm.get_pose();
+
+		xDiff =  math.abs((oldX - v.x) / ((oldX + v.x)/2));
+		yDiff =  math.abs((oldY - v.y) / ((oldY + v.y)/2));
+
+		avgDiff = (xDiff + yDiff)/2;
+
+		print("Average difference between localize: ");
+		print(avgDiff);
+
+		-- If I consistently think I am in the same place
+		if(avgDiff < 5)  then
+			return faceXY_b	
+		else
+			return localize_b;
+		end
   end,
 
   [faceXY_b] = function() -- if we're in faceXY
-    vector = wcm.get_pose();
-    x = vector.x;
-    y = vector.y;
-    a = vector.a;
+    v = wcm.get_pose();
+    x = v.x;
+    y = v.y;
+    a = v.a;
 
     deltaX = xPrime - x;
     deltaY = yPrime - y;
 
     aPrime = math.atan2(deltaY, deltaX);
 
-    aPrime = aPrime * (180/math.pi);
+		print("Current angle: ");
+		print(aPrime);
 
-    if wcm.get_pose().a == aPrime then
+    --aPrime = aPrime * (180/math.pi);
+
+    if math.abs(wcm.get_pose().a - aPrime) < 0.5 then
       return moveToXY_b
     else
       return faceXY_b
@@ -106,15 +154,17 @@ machine = makeHFA("machine", makeTransition({
   end,
 
   [moveToXY_b] = function()
-    vector = wcm.get_pose();
-    if vector.x == x and vector.y = y then
+    v = wcm.get_pose();
+    if v.x == x and v.y == y then
       return stopRobot_b;
     elseif counter == 100 then
       counter = 0;
       return localize_b;
     else
-      counter += 1;
+      counter = counter + 1;
       return moveToXY_b;
     end
   end
 }))
+
+darwin.executeMachine(machine);
